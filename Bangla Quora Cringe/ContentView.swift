55851c0e3f13107c98c44e659ca1f71cf1939e96
Shawon Ashraf <shawon13@live.com>
2019-10-25 23:43:58 +0200

    init

---
+++ Bangla Quora Cringe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bangla Quora Cringe
+//
+//  Created by Shawon Ashraf on 10/25/19.
+//  Copyright © 2019 Shawon Ashraf. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
